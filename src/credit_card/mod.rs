46a2ea7916723f2d5ea794aae989c18bd97e1a2d
Markus Iorio <mbalzert@gmail.com>
2024-01-03 22:10:59 +0100

    credit card second iteration

--- src/credit_card/mod.rs
+++ src/credit_card/mod.rs
@@ -9,66 +9,86 @@ impl CreditCard {
         }
     }
     pub fn is_valid(&self) -> bool {
-        self.is_valid_format() && self.is_dividable_by_ten() && self.is_dividable_by_two()
+        self.check_format() && self.check_sum10() && self.check_sum2()
     }
-    fn is_dividable_by_ten(&self) -> bool {
-        let total_sum: u32 = self
-            .number
-            .chars()
-            .filter(|c| c.is_ascii_digit())
-            .map(|c| c.to_digit(10).unwrap())
-            .sum();
-
-        total_sum % 10 == 0
+    pub fn check_sum10(&self) -> bool {
+        let sum: u32 = self.number.chars().filter_map(|c| c.to_digit(10)).sum();
+        sum % 10 == 0
     }
-    fn is_dividable_by_two(&self) -> bool {
-        let total_sum: u32 = self
+    fn check_sum2(&self) -> bool {
+        let sum: u32 = self
             .number
             .split_whitespace()
-            .flat_map(|block| block.chars().filter(|c| c.is_ascii_digit()))
-            .map(|c| c.to_digit(10).unwrap())
+            .flat_map(|s| s.chars().filter_map(|c| c.to_digit(10)))
             .sum();
-
-        total_sum % 2 == 0
+        sum % 2 == 0
     }
-    fn is_valid_format(&self) -> bool {
-        let blocks = self
-            .number
-            .chars()
-            .filter(|i| !i.is_whitespace())
-            .collect::<Vec<char>>();
-        if !is_valid_lenght(&blocks) {
+    fn check_format(&self) -> bool {
+        let blocks: Vec<&str> = self.number.split(' ').collect();
+        let numbers = self.number.replace(' ', "");
+        if !is_valid_length(&blocks, 4) {
             return false;
         }
-        if !is_digits(&blocks) {
+        if !is_valid_length(&numbers, 16) {
             return false;
         }
-        if is_all_zero(&blocks) {
+        if !is_numeric(&numbers) {
+            return false;
+        }
+        if !is_valid_numbers(&numbers) {
             return false;
         }
         true
     }
 }
 
-fn is_all_zero(value: &[char]) -> bool {
-    if let Some(numbers) = parse_to_digit(value) {
-        numbers.into_iter().all(|i| i == 0)
-    } else {
-        false
+trait HasLength {
+    fn len(&self) -> usize;
+}
+
+impl HasLength for String {
+    fn len(&self) -> usize {
+        self.len()
     }
 }
-
-fn is_digits(value: &[char]) -> bool {
-    parse_to_digit(value).is_some()
+impl HasLength for Vec<&str> {
+    fn len(&self) -> usize {
+        self.len()
+    }
+}
+fn is_valid_length<T: HasLength>(value: &T, count: usize) -> bool {
+    value.len() == count
 }
 
-fn parse_to_digit(value: &[char]) -> Option<Vec<u32>> {
-    value
-        .iter()
-        .map(|i| i.to_digit(10))
-        .collect::<Option<Vec<u32>>>()
+fn is_numeric(value: &str) -> bool {
+    value.chars().all(|c| c.is_ascii_digit())
+}
+fn is_valid_numbers(value: &str) -> bool {
+    value.chars().filter_map(|c| c.to_digit(10)).any(|d| d > 0)
 }
 
-fn is_valid_lenght(value: &Vec<char>) -> bool {
-    value.len() == 16
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_valid_card_should_be_true() {
+        let card = CreditCard::new("1234 5678 9012 3450");
+        assert!(card.is_valid());
+    }
+
+    #[test]
+    fn test_all_zero_should_be_false() {
+        assert!(!is_valid_numbers("0000"));
+    }
+    #[test]
+    fn test_check_sum10_should_be_true() {
+        let card = CreditCard::new("37");
+        assert!(card.check_sum10());
+    }
+    #[test]
+    fn test_check_sum2_should_be_true() {
+        let card = CreditCard::new("4 8");
+        assert!(card.check_sum2());
+    }
 }
